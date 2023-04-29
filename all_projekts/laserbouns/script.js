21abac63d978c2983605a94c7e7e7e42fd42838a
ttvchrisklar <christiankaatorp@gmail.com>
2023-04-29 12:48:30 +0200

    new page

--- all_projekts/laserbouns/script.js
+++ all_projekts/laserbouns/script.js
@@ -3,3 +3,52 @@ const ctx = document.getElementById("canvas").getContext("2d"),
 var canvasHeight = document.getElementById("canvas").height,
     canvasWidth = document.getElementById("canvas").width;
 
+var blockPos = {
+    0: {
+        x: 0,
+        y: 0,
+    },
+    1: {
+        x: 100,
+        y: 0,
+    },
+    2: {
+        x: 200,
+        y: 0,
+    },
+    3: {
+        x: 0,
+        y: 100,
+    },
+    4: {
+        x: 100,
+        y: 100,
+    },
+    5: {
+        x: 200,
+        y: 100,
+    },
+    6: {
+        x: 0,
+        y: 200,
+    },
+    7: {
+        x: 100,
+        y: 200,
+    },
+    8: {
+        x: 200,
+        y: 200,
+    },
+};
+function draw() {
+    for (let i = 0; i < 9; i++) {
+        var curntBlock = blockPos[i];
+        ctx.beginPath();
+        ctx.rect(curntBlock.x, curntBlock.y, 100, 100);
+        ctx.fillStyle = "black";
+        ctx.stroke();
+        console.log(blockPos[i]);
+    }
+}
+window.onload = draw();
